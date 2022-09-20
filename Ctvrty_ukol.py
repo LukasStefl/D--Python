10185fd31360378abe99f4f6bc67699556dbff8a
LukasStefl <lukas.stefl@student.ossp.cz>
2022-09-20 11:28:34 +0200

    commit

--- Ctvrty_ukol.py
+++ Ctvrty_ukol.py
@@ -1,6 +1,11 @@
 numbers = [12,75,150,180,145,525]
 for x in numbers:
     if x % 5 == 0:
+        if x > 150:
+            continue
+        if x > 500:
+            break
         print(x);
+
 
   
